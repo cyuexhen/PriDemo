5def0924dab58f0c4284e27e80e183404cec1d5f
ST DONG <cyuexhen@outlook.com>
2016-12-24 01:01:58 +0800

    Release 1.0

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(args != null);
+
+            Console.Write("main");
+
+            Console.ReadKey();
         }
     }
 }
