acbc5efcf71b45a8c77ba2b9a2fb71ce960eefb8
ST DONG <cyuexhen@outlook.com>
2016-12-23 13:44:30 +0800

    调整类类型

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("main");
         }
     }
 }
